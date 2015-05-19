e75315ee6c3824b41b77b56b842540f4b4b50819
Dwayne Jeng <dwayne.jeng@sagebase.org>
2015-05-18 19:10:16 -0700

    append row pagination, system.exit(0) call in finally to force close

--- src/main/java/org/sagebionetworks/bridge/scripts/PdMomentInDay.java
+++ src/main/java/org/sagebionetworks/bridge/scripts/PdMomentInDay.java
@@ -50,6 +50,7 @@ import org.sagebionetworks.bridge.util.BridgeExporterUtil;
  */
 public class PdMomentInDay {
     private static final long APPEND_TIMEOUT_MILLISECONDS = 30 * 1000;
+    private static final int APPEND_PAGE_SIZE = 64;
     private static final String MOMENT_IN_DAY_FORMAT_JSON = "momentInDayFormat.json";
     private static final String MOMENT_IN_DAY_FORMAT_JSON_CHOICE_ANSWERS = "momentInDayFormat.json.choiceAnswers";
     private static final int TASK_REMAINING_REPORT_INTERVAL = 50;
@@ -65,175 +66,186 @@ public class PdMomentInDay {
     private final static Map<String, TableMetadata> tableMetadataMap = new HashMap<>();
 
     public static void main(String[] args) throws InterruptedException, IOException, SynapseException {
-        // args
-        if (args.length < 2) {
-            throw new IllegalArgumentException("Usage: PdMomentInDay [upload date] [[synapse table ID]...]");
-        }
-        String uploadDate = args[0];
-        tableIdArray = Arrays.copyOfRange(args, 1, args.length);
+        try {
+            // args
+            if (args.length < 2) {
+                throw new IllegalArgumentException("Usage: PdMomentInDay [upload date] [[synapse table ID]...]");
+            }
+            String uploadDate = args[0];
+            tableIdArray = Arrays.copyOfRange(args, 1, args.length);
 
-        // init config
-        File synapseConfigFile = new File(System.getProperty("user.home") + "/bridge-synapse-exporter-config.json");
-        BridgeExporterConfig config = BridgeExporterUtil.JSON_MAPPER.readValue(synapseConfigFile,
-                BridgeExporterConfig.class);
+            // init config
+            File synapseConfigFile = new File(System.getProperty("user.home") + "/bridge-synapse-exporter-config.json");
+            BridgeExporterConfig config = BridgeExporterUtil.JSON_MAPPER.readValue(synapseConfigFile,
+                    BridgeExporterConfig.class);
 
-        // init synapse client
-        synapseClient = new SynapseClientImpl();
-        synapseClient.setUserName(config.getUsername());
-        synapseClient.setApiKey(config.getApiKey());
+            // init synapse client
+            synapseClient = new SynapseClientImpl();
+            synapseClient.setUserName(config.getUsername());
+            synapseClient.setApiKey(config.getApiKey());
 
-        // init synapse helper with synapse client
-        SynapseHelper synapseHelper = new SynapseHelper();
-        synapseHelper.setSynapseClient(synapseClient);
+            // init synapse helper with synapse client
+            SynapseHelper synapseHelper = new SynapseHelper();
+            synapseHelper.setSynapseClient(synapseClient);
 
-        // executor
-        ExecutorService executor = Executors.newFixedThreadPool(config.getNumThreads());
+            // executor
+            ExecutorService executor = Executors.newFixedThreadPool(config.getNumThreads());
 
-        // query synapse for each table to get its parent project ID and column info
-        System.out.println("Initializing table metadata...");
-        for (String oneTableId : tableIdArray) {
-            TableMetadata oneTableMetadata = new TableMetadata();
+            // query synapse for each table to get its parent project ID and column info
+            System.out.println("Initializing table metadata...");
+            for (String oneTableId : tableIdArray) {
+                TableMetadata oneTableMetadata = new TableMetadata();
 
-            // get parent ID
-            TableEntity oneTable = synapseHelper.getTableWithRetry(oneTableId);
-            oneTableMetadata.parentId = oneTable.getParentId();
+                // get parent ID
+                TableEntity oneTable = synapseHelper.getTableWithRetry(oneTableId);
+                oneTableMetadata.parentId = oneTable.getParentId();
 
-            // get column data
-            List<ColumnModel> columnList = synapseClient.getColumnModelsForTableEntity(oneTableId);
-            for (ColumnModel oneColumn : columnList) {
-                String columnName = oneColumn.getName();
+                // get column data
+                List<ColumnModel> columnList = synapseClient.getColumnModelsForTableEntity(oneTableId);
+                for (ColumnModel oneColumn : columnList) {
+                    String columnName = oneColumn.getName();
 
-                // columns that start with momentInDayFormat.json are moment in day columns
-                if (columnName.startsWith(MOMENT_IN_DAY_FORMAT_JSON)) {
-                    oneTableMetadata.momentInDayColumnSet.add(columnName);
-                    oneTableMetadata.columnNameToId.put(columnName, oneColumn.getId());
+                    // columns that start with momentInDayFormat.json are moment in day columns
+                    if (columnName.startsWith(MOMENT_IN_DAY_FORMAT_JSON)) {
+                        oneTableMetadata.momentInDayColumnSet.add(columnName);
+                        oneTableMetadata.columnNameToId.put(columnName, oneColumn.getId());
+                    }
+
+                    // if it's one of these two, it's the column we're query keying on
+                    if (columnName.equals(MOMENT_IN_DAY_FORMAT_JSON)
+                            || columnName.equals(MOMENT_IN_DAY_FORMAT_JSON_CHOICE_ANSWERS)) {
+                        oneTableMetadata.queryColumn = columnName;
+                    }
                 }
 
-                // if it's one of these two, it's the column we're query keying on
-                if (columnName.equals(MOMENT_IN_DAY_FORMAT_JSON)
-                        || columnName.equals(MOMENT_IN_DAY_FORMAT_JSON_CHOICE_ANSWERS)) {
-                    oneTableMetadata.queryColumn = columnName;
+                if (Strings.isNullOrEmpty(oneTableMetadata.queryColumn)
+                        || oneTableMetadata.momentInDayColumnSet.isEmpty()) {
+                    throw new IllegalStateException(
+                            "Synapse table " + oneTableId + " doesn't have moment in day fields");
+                }
+
+                tableMetadataMap.put(oneTableId, oneTableMetadata);
+            }
+
+            // Any table may have the momentInDay, so we need to check all of them.
+            Queue<Future<?>> outstandingTaskQueue = new LinkedList<>();
+            int numRows = 0;
+            for (String oneTableId : tableIdArray) {
+                System.out.println("Scanning table " + oneTableId);
+                TableMetadata oneTableMetadata = tableMetadataMap.get(oneTableId);
+
+                // always include recordId, healthCode, and createdOn
+                Set<String> selectColumnSet = new HashSet<>();
+                selectColumnSet.add("recordId");
+                selectColumnSet.add("healthCode");
+                selectColumnSet.add("createdOn");
+                selectColumnSet.addAll(oneTableMetadata.momentInDayColumnSet);
+                String sql = "SELECT \"" + SELECT_COLUMN_JOINER.join(selectColumnSet) + "\" FROM " + oneTableId
+                        + " WHERE \"" + oneTableMetadata.queryColumn + "\" IS NOT NULL AND uploadDate='" + uploadDate
+                        + "'";
+
+                // Iterate table results
+                SynapseTableIterator tableIter = new SynapseTableIterator(synapseClient, sql, oneTableId);
+                List<SelectColumn> selectColumnList = tableIter.getHeaders();
+                int numColumns = selectColumnList.size();
+                while (tableIter.hasNext()) {
+                    Row rowWithMomentInDay = tableIter.next();
+                    Task task = new Task();
+                    task.numColumns = numColumns;
+                    task.row = rowWithMomentInDay;
+                    task.selectColumnList = selectColumnList;
+                    task.tableId = oneTableId;
+                    Future<?> taskFuture = executor.submit(task);
+                    outstandingTaskQueue.add(taskFuture);
                 }
             }
 
-            if (Strings.isNullOrEmpty(oneTableMetadata.queryColumn)
-                    || oneTableMetadata.momentInDayColumnSet.isEmpty()) {
-                throw new IllegalStateException("Synapse table " + oneTableId + " doesn't have moment in day fields");
-            }
-
-            tableMetadataMap.put(oneTableId, oneTableMetadata);
-        }
-
-        // Any table may have the momentInDay, so we need to check all of them.
-        Queue<Future<?>> outstandingTaskQueue = new LinkedList<>();
-        for (String oneTableId : tableIdArray) {
-            System.out.println("Scanning table " + oneTableId);
-            TableMetadata oneTableMetadata = tableMetadataMap.get(oneTableId);
-
-            // always include recordId, healthCode, and createdOn
-            Set<String> selectColumnSet = new HashSet<>();
-            selectColumnSet.add("recordId");
-            selectColumnSet.add("healthCode");
-            selectColumnSet.add("createdOn");
-            selectColumnSet.addAll(oneTableMetadata.momentInDayColumnSet);
-            String sql = "SELECT \"" + SELECT_COLUMN_JOINER.join(selectColumnSet) + "\" FROM " + oneTableId
-                    + " WHERE \"" + oneTableMetadata.queryColumn + "\" IS NOT NULL AND uploadDate='" + uploadDate
-                    + "'";
-
-            // Iterate table results
-            SynapseTableIterator tableIter = new SynapseTableIterator(synapseClient, sql, oneTableId);
-            List<SelectColumn> selectColumnList = tableIter.getHeaders();
-            int numColumns = selectColumnList.size();
-            while (tableIter.hasNext()) {
-                Row rowWithMomentInDay = tableIter.next();
-                Task task = new Task();
-                task.numColumns = numColumns;
-                task.row = rowWithMomentInDay;
-                task.selectColumnList = selectColumnList;
-                task.tableId = oneTableId;
-                Future<?> taskFuture = executor.submit(task);
-                outstandingTaskQueue.add(taskFuture);
-            }
-        }
-
-        // Wait for outstanding tasks
-        Stopwatch outstandingTaskStopwatch = Stopwatch.createStarted();
-        while (!outstandingTaskQueue.isEmpty()) {
-            int numOutstanding = outstandingTaskQueue.size();
-            if (numOutstanding % TASK_REMAINING_REPORT_INTERVAL == 0) {
-                System.out.println("Num outstanding tasks: " + numOutstanding + " after " +
-                        outstandingTaskStopwatch.elapsed(TimeUnit.SECONDS) + " seconds");
-            }
-
-            // Tasks have no return value. If Future.get() returns normally, then the task is done.
-            Future<?> oneFuture = outstandingTaskQueue.remove();
-            try {
-                oneFuture.get();
-            } catch (ExecutionException | InterruptedException ex) {
-                System.out.println("[ERROR] Error finishing task: " + ex.getMessage());
-                ex.printStackTrace(System.out);
-            }
-        }
-
-        // Report
-        for (String oneTableId : tableIdArray) {
-            TableMetadata oneTableMetadata = tableMetadataMap.get(oneTableId);
-            System.out.println("Rows to update in " + oneTableId + ": " + oneTableMetadata.rowUpdateList.size());
-        }
-
-        // Update rows
-        for (String oneTableId : tableIdArray) {
-            System.out.println("Updating table " + oneTableId);
-            TableMetadata oneTableMetadata = tableMetadataMap.get(oneTableId);
-            if (oneTableMetadata.rowUpdateList.isEmpty()) {
-                // Skip
-                continue;
-            }
-
-            List<PartialRow> partialRowList = new ArrayList<>();
-            for (TableRowUpdate oneRowUpdate : oneTableMetadata.rowUpdateList) {
-                System.out.println("Updating record ID " + oneRowUpdate.recordId);
-
-                // special handling if we need to upload the JSON file
-                if (oneRowUpdate.momentInDayJsonBytes != null) {
-                    // upload the JSON file as a file handle
-                    File tmpFile = File.createTempFile("momentInDayFormat", ".json");
-                    Files.write(oneRowUpdate.momentInDayJsonBytes, tmpFile);
-                    FileHandle fileHandle = synapseHelper.createFileHandleWithRetry(tmpFile, "text/json",
-                            oneTableMetadata.parentId);
-                    tmpFile.delete();
-
-                    // write that file handle ID to the map of columns
-                    String fileHandleId = fileHandle.getId();
-                    oneRowUpdate.columnValueMap.put(MOMENT_IN_DAY_FORMAT_JSON, fileHandleId);
+            // Wait for outstanding tasks
+            Stopwatch outstandingTaskStopwatch = Stopwatch.createStarted();
+            while (!outstandingTaskQueue.isEmpty()) {
+                int numOutstanding = outstandingTaskQueue.size();
+                if (numOutstanding % TASK_REMAINING_REPORT_INTERVAL == 0) {
+                    System.out.println("Num outstanding tasks: " + numOutstanding + " after " +
+                            outstandingTaskStopwatch.elapsed(TimeUnit.SECONDS) + " seconds");
                 }
 
-                // construct a map of column ID to value instead of name to value (since append rows takes column IDs,
-                // not names)
-                Map<String, String> columnIdToValue = new HashMap<>();
-                for (Map.Entry<String, String> columnNameValuePair : oneRowUpdate.columnValueMap.entrySet()) {
-                    String columnName = columnNameValuePair.getKey();
-                    String columnValue = columnNameValuePair.getValue();
-                    String columnId = oneTableMetadata.columnNameToId.get(columnName);
-                    columnIdToValue.put(columnId, columnValue);
+                // Tasks have no return value. If Future.get() returns normally, then the task is done.
+                Future<?> oneFuture = outstandingTaskQueue.remove();
+                try {
+                    oneFuture.get();
+                } catch (ExecutionException | InterruptedException ex) {
+                    System.out.println("[ERROR] Error finishing task: " + ex.getMessage());
+                    ex.printStackTrace(System.out);
                 }
-
-                // make partial row set
-                PartialRow partialRow = new PartialRow();
-                partialRow.setRowId(oneRowUpdate.rowId);
-                partialRow.setValues(columnIdToValue);
-                partialRowList.add(partialRow);
             }
 
-            // batch update rows
-            PartialRowSet partialRowSet = new PartialRowSet();
-            partialRowSet.setTableId(oneTableId);
-            partialRowSet.setRows(partialRowList);
-            synapseClient.appendRowsToTable(partialRowSet, APPEND_TIMEOUT_MILLISECONDS, oneTableId);
-        }
+            // Report
+            for (String oneTableId : tableIdArray) {
+                TableMetadata oneTableMetadata = tableMetadataMap.get(oneTableId);
+                System.out.println("Rows to update in " + oneTableId + ": " + oneTableMetadata.rowUpdateList.size());
+            }
 
-        System.out.println("Done");
-        System.exit(0);
+            // Update rows
+            for (String oneTableId : tableIdArray) {
+                System.out.println("Updating table " + oneTableId);
+                TableMetadata oneTableMetadata = tableMetadataMap.get(oneTableId);
+                if (oneTableMetadata.rowUpdateList.isEmpty()) {
+                    // Skip
+                    continue;
+                }
+
+                int numRowsToUpdate = oneTableMetadata.rowUpdateList.size();
+                for (int i = 0; i < numRowsToUpdate; i += APPEND_PAGE_SIZE) {
+                    int endIdx = Math.min(numRowsToUpdate, i + APPEND_PAGE_SIZE);
+                    List<TableRowUpdate> rowUpdatePage = oneTableMetadata.rowUpdateList.subList(i, endIdx);
+
+                    List<PartialRow> partialRowList = new ArrayList<>();
+                    for (TableRowUpdate oneRowUpdate : rowUpdatePage) {
+                        // special handling if we need to upload the JSON file
+                        if (oneRowUpdate.momentInDayJsonBytes != null) {
+                            // upload the JSON file as a file handle
+                            File tmpFile = File.createTempFile("momentInDayFormat", ".json");
+                            Files.write(oneRowUpdate.momentInDayJsonBytes, tmpFile);
+                            FileHandle fileHandle = synapseHelper.createFileHandleWithRetry(tmpFile, "text/json",
+                                    oneTableMetadata.parentId);
+                            tmpFile.delete();
+
+                            // write that file handle ID to the map of columns
+                            String fileHandleId = fileHandle.getId();
+                            oneRowUpdate.columnValueMap.put(MOMENT_IN_DAY_FORMAT_JSON, fileHandleId);
+                        }
+
+                        // construct a map of column ID to value instead of name to value (since append rows takes
+                        // column
+                        // IDs,
+                        // not names)
+                        Map<String, String> columnIdToValue = new HashMap<>();
+                        for (Map.Entry<String, String> columnNameValuePair : oneRowUpdate.columnValueMap.entrySet()) {
+                            String columnName = columnNameValuePair.getKey();
+                            String columnValue = columnNameValuePair.getValue();
+                            String columnId = oneTableMetadata.columnNameToId.get(columnName);
+                            columnIdToValue.put(columnId, columnValue);
+                        }
+
+                        // make partial row set
+                        PartialRow partialRow = new PartialRow();
+                        partialRow.setRowId(oneRowUpdate.rowId);
+                        partialRow.setValues(columnIdToValue);
+                        partialRowList.add(partialRow);
+                    }
+
+                    // batch update rows
+                    PartialRowSet partialRowSet = new PartialRowSet();
+                    partialRowSet.setTableId(oneTableId);
+                    partialRowSet.setRows(partialRowList);
+                    synapseClient.appendRowsToTable(partialRowSet, APPEND_TIMEOUT_MILLISECONDS, oneTableId);
+                }
+            }
+
+            System.out.println("Done");
+        } finally {
+            System.exit(0);
+        }
     }
 
     // This class (really a struct) tracks table metadata needed for constructing the queries to Synapse, including
