417e056981f6380625a97b0bab91f7fd303b7749
Dwayne Jeng <dwayne.jeng@sagebase.org>
2016-04-22 01:25:04 -0700

    add heartbeat logging

--- src/main/java/org/sagebionetworks/bridge/exporter/config/WorkerLauncher.java
+++ src/main/java/org/sagebionetworks/bridge/exporter/config/WorkerLauncher.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import org.sagebionetworks.bridge.heartbeat.HeartbeatLogger;
 import org.sagebionetworks.bridge.sqs.PollSqsWorker;
 
 /**
@@ -16,7 +17,13 @@ import org.sagebionetworks.bridge.sqs.PollSqsWorker;
 public class WorkerLauncher implements CommandLineRunner {
     private static final Logger LOG = LoggerFactory.getLogger(WorkerLauncher.class);
 
+    private HeartbeatLogger heartbeatLogger;
     private PollSqsWorker sqsWorker;
+
+    @Autowired
+    public final void setHeartbeatLogger(HeartbeatLogger heartbeatLogger) {
+        this.heartbeatLogger = heartbeatLogger;
+    }
 
     // This is singleton, because the bottleneck is how many threads we can spin up to handle one export job, not how
     // many export jobs we can do in parallel. If we for some reason find we need to do parallel export jobs, we can
@@ -34,7 +41,10 @@ public class WorkerLauncher implements CommandLineRunner {
      */
     @Override
     public void run(String... args) {
+        LOG.info("Starting heartbeat...");
+        new Thread(heartbeatLogger).start();
+
         LOG.info("Starting worker...");
-        sqsWorker.run();
+        new Thread(sqsWorker).start();
     }
 }
